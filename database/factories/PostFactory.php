1147634d679c6e6275a3f33784528adf6c9b66fc
kanashiro-igor <kanashiro.igor@gmail.com>
2022-01-11 18:18:24 -0300

    Blog em progresso

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Category;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'category_id' => Category::factory(),
+            'title' => $this->faker->sentence(),
+            'slug' => $this->faker->slug(),
+            'excerpt' => collect($this->faker->paragraphs(2))->map(fn($item) => "<p>$item</p>")->implode(''),
+            'body' => collect($this->faker->paragraphs(6))->map(fn($item) => "<p>$item</p>")->implode('')
+        ];
+    }
+}
