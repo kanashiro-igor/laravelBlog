a708e14e8e9c17da790059a24314da979b7c6994
kanashiro-igor <kanashiro.igor@gmail.com>
2022-01-12 08:16:27 -0300

    Alterando rotas e criando controllers.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Category;
+
+
+class UserController extends Controller
+{
+    public function show (User $author)
+    {
+        return view('posts', [
+            'posts' => $author->posts,
+            'categories' => Category::all()
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\User;
@@ -20,23 +22,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
-
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
+Route::get('categories/{category:slug}', [CategoryController::class, 'show'])->name('category');
 
-Route::get('categories/{category:slug}', function (Category $category) {
-
-    return view('posts', [
-        'posts' => $category->posts,
-        'currentCategory' => $category,
-        'categories' => Category::all()
-    ]);
-})->name('category');
-
-Route::get('authors/{author:username}', function (User $author) {
-
-    return view('posts', [
-        'posts' => $author->posts,
-        'categories' => Category::all()
-    ]);
-});
+Route::get('authors/{author:username}', [UserController::class, 'show']);
