c193097387298468e02de4087f53b4c9a875f3df
kanashiro-igor <kanashiro.igor@gmail.com>
2022-01-21 20:26:37 -0300

    Making commentaries dynamic

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -3,6 +3,9 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
 
 class CommentFactory extends Factory
 {
@@ -14,7 +17,9 @@ class CommentFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'post_id' => Post::factory(),
+            'user_id' => User::factory(),
+            'body' => $this->faker->paragraph()
         ];
     }
 }
--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,24 +1,22 @@
+@props(['comment'])
+
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/100" alt="" width="60" height="60" class="rounded-xl">
+        <img src="https://i.pravatar.cc/60?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
     </div>
 
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">Teste Testarius</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
 
             <p class="text-xs">
                 Posted
-                <time>8 months ago</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
         </header>
 
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam placerat fermentum eros, non lobortis nisl euismod vel.
-            Nullam dictum rutrum tincidunt.
-            Integer ac lectus facilisis, iaculis ex eu, ultricies nulla. Nullam ultrices enim sed elementum blandit.
-            Suspendisse imperdiet, ipsum tempus pretium placerat,
-            arcu ligula eleifend massa, non aliquet risus nisi quis magna.
+            {{ $comment->body }}
         </p>
     </div>
 </article>
