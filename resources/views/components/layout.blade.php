1bfff80c22e94f00ee8b18c355c8359084bf2aaa
kanashiro-igor <kanashiro.igor@gmail.com>
2022-02-04 11:37:11 -0300

    Added Newsletter with Mail Chimp

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,6 +6,22 @@
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 <script defer src="https://unpkg.com/alpinejs@3.7.1/dist/cdn.min.js"></script>
 
+<style>
+    html {
+        scroll-behavior: smooth;
+    }
+
+    clamp {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+    }
+
+    clamp.one-line {
+        -webkit-line-clamp: 1   ;
+    }
+</style>
+
 <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
         <nav class="md:flex md:justify-between md:items-center">
@@ -31,7 +47,7 @@
 
                 <!-- <a href="/" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5 hover:bg-blue-700">Home Page</a> -->
 
-                <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5 hover:bg-blue-700">
+                <a href="#newsletter" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-6 hover:bg-blue-700">
                     Subscribe for Updates
                 </a>
             </div>
@@ -47,16 +63,24 @@
             <div class="mt-10">
                 <div class="relative inline-block mx-auto lg:bg-gray-200 rounded-full">
 
-                    <form method="POST" action="#" class="lg:flex text-sm">
+                    <form method="POST" action="/newsletter" class="lg:flex text-sm">
+                        @csrf
                         <div class="lg:py-3 lg:px-5 flex items-center">
                             <label for="email" class="hidden lg:inline-block">
                                 <img src="/images/mailbox-icon.svg" alt="mailbox letter">
                             </label>
 
-                            <input id="email" type="text" placeholder="Your email address" class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
+                            <div>
+                                <input id="email" name="email" type="text" placeholder="Your email address" class="lg:bg-transparent py-2 lg:py-0 pl-4 focus-within:outline-none">
+                                @error('email')
+                                    <p class="text-red-500 text-sm mt-1"> {{ $message }} </p>
+                                @enderror
+                            </div>
                         </div>
-
-                        <button type="submit" class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-8">
+                        
+                        <button id="newsletter" type="submit" 
+                            class="transition-colors duration-300 bg-blue-500 hover:bg-blue-600 mt-4 lg:mt-0 lg:ml-3 rounded-full text-xs font-semibold text-white 
+                                    uppercase py-3 px-8">
                             Subscribe
                         </button>
                     </form>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,32 @@ use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/newsletter', function () {
+
+    request()->validate(['email' => ['required'], ['email']]);
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us14'
+    ]);
+
+    try {
+        $response = $mailchimp->lists->addListMember("a509c76680", [
+            "email_address" => 'igorkanashirodesousa1995@gmail.com',
+            "status" => 'subscribed'
+        ]);
+    } catch (\Exception $e) {
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'Your e-mail could not be added to our newsletter list'
+        ]);
+    }
+
+    return  redirect('/')
+        ->with('success', 'You are now signed in for our newsletter!');
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
@@ -18,12 +44,12 @@ Route::middleware('guest')->group(function () {
     Route::get('login', [SessionsController::class, 'create']);
     Route::post('login', [SessionsController::class, 'store']);
 });
-    
+
 // Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 
 // Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
-Route::middleware('auth')->group(function() {
+Route::middleware('auth')->group(function () {
     Route::post('logout', [SessionsController::class, 'destroy']);
     Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 });
